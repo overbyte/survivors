1ba63139a7fd20954b83aee6d7658a4098461283
Allandt Bik-Elliott <allandt@gmail.com>
2025-01-30 14:40:35 +0000

    fix on_died error which occurs when trying to refer to the owner when theyre already removed from memory

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -12,6 +12,11 @@ func _ready():
 	
 func damage(damage_amount: float):
 	current_health = max(current_health - damage_amount, 0)
+	# this calls the check_death on the next idle frame
+	# to fix the error vial_drop_component.gd:18 @ on_died(): Can't change this state while flushing queries. Use call_deferred() or set_deferred() to change monitoring state instead.
+	Callable(check_death).call_deferred()
+
+func check_death():
 	if current_health == 0:
 		died.emit()
 		owner.queue_free()
