3f2be67f9960e846eef8426f907e8b316dd4ec2a
Allandt Bik-Elliott <allandt@gmail.com>
2025-01-30 16:46:22 +0000

    add a drop percent to set how often the vial drops after an enemy dies

--- scenes/component/vial_drop_component.gd
+++ scenes/component/vial_drop_component.gd
@@ -1,7 +1,8 @@
 extends Node
 
-@export var vial_scene: PackedScene
+@export_range(0, 1) var drop_percent: float = 0.5
 @export var health_component: Node
+@export var vial_scene: PackedScene
 
 func _ready():
 	(health_component as HealthComponent).died.connect(on_died)
